d42e4013dea42780ea06244dcf665ae1693ea448
liziwei <1464050900@qq.com>
2026-06-05 18:34:31 +0800

    01

---
+++ langchain_study/langchain01.py
@@ -0,0 +1,13 @@
+import os
+#os.environ["DEEPSEEK_API_KEY"] = "sk-124c54d6772a44cbbf2440092d9752c7"
+
+from langchain.agents import create_agent
+
+agent = create_agent(
+    model="deepseek-v4-flash",
+    system_prompt="你是一位小助手"
+)
+res = agent.invoke(
+    {"message": [{"role":"user", "content":"天气怎么样"}]}
+)
+print(res)
